c309e87b1e80734bd7766361192ac39784c54ba2
Arinji2 <arinjaydhar205@gmail.com>
2025-06-11 19:56:37 +0530

    Finish Contact

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,15 @@
+import Contact from "./contact";
 import Featured from "./featured";
 import Hero from "./hero";
 import Timeline from "./timeline";
 
 export default function Home() {
   return (
-    <div className="w-full gap-20 pb-10 h-fit max-w-pageMax md:py-6 flex flex-col items-center justify-start">
+    <div className=" font-content w-full gap-20 pb-10 h-fit max-w-pageMax md:py-6 flex flex-col items-center justify-start">
       <Hero />
       <Featured />
       <Timeline />
+      <Contact />
     </div>
   );
 }
