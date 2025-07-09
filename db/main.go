b2c66cc22fa71e6dfc8bdec4e0d065b5778938a0
Arinji2 <arinjaydhar205@gmail.com>
2025-07-09 12:22:17 +0530

    Utilize keypairs.json in init

--- db/main.go
+++ db/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	_ "embed"
+	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 	"strings"
@@ -12,10 +15,18 @@ import (
 	"github.com/pocketbase/pocketbase/plugins/migratecmd"
 )
 
+//go:embed dist/keypairs.json
+var keypairsData []byte
+var KeyPairs map[string]string
+
 func main() {
 	app := pocketbase.New()
-
 	isGoRun := strings.HasPrefix(os.Args[0], os.TempDir())
+
+	if err := json.Unmarshal(keypairsData, &KeyPairs); err != nil {
+		log.Fatal("Error parsing keypairs:", err)
+	}
+
 	frontendURL := os.Getenv("FRONTEND_URL")
 	if frontendURL == "" {
 		frontendURL = "http://localhost:3000"
@@ -32,14 +43,45 @@ func main() {
 	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
 		Automigrate: isGoRun,
 	})
+
+	app.OnServe().BindFunc(func(e *core.ServeEvent) error {
+		fmt.Println("Server started! Initializing keys...")
+
+		collection, err := app.FindCollectionByNameOrId("keys")
+		if err != nil {
+			log.Printf("Error finding collection: %v", err)
+			return err
+		}
+
+		for keyName, keyValue := range KeyPairs {
+			fmt.Printf("Checking key: %s\n", keyName)
+			exists, err := app.FindRecordsByFilter(collection.Id, fmt.Sprintf("key='%s'", keyName), "", 1, 0)
+			if err != nil {
+				log.Printf("Error finding record: %v", err)
+				continue
+			}
+
+			if len(exists) == 0 {
+				log.Printf("Creating key: %s\n", keyName)
+				record := core.NewRecord(collection)
+				record.Set("key", keyName)
+				record.Set("value", keyValue)
+
+				if err := app.Save(record); err != nil {
+					log.Printf("Error creating record: %v", err)
+				}
+			}
+		}
+
+		return e.Next()
+	})
+
 	app.OnRecordUpdateRequest("music").BindFunc(func(e *core.RecordRequestEvent) error {
 		client := api.NewAPIClient(frontendURL)
 		_, err := client.SendRequestWithQuery("GET", "/api/revalidate", map[string]string{"secret": secret}, map[string]string{})
 		if err != nil {
-			log.Fatal("Error:", err)
-			return nil
+			log.Printf("Revalidation error: %v", err)
 		}
-
 		return e.Next()
 	})
 
@@ -47,11 +89,12 @@ func main() {
 		client := api.NewAPIClient(frontendURL)
 		_, err := client.SendRequestWithQuery("GET", "/api/revalidate", map[string]string{"secret": secret}, map[string]string{})
 		if err != nil {
-			log.Fatal("Error:", err)
-			return nil
+			log.Printf("Revalidation error: %v", err)
 		}
 		return e.Next()
 	})
+
+	fmt.Println("Starting Server...")
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
 	}
