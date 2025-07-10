e0585e958dc400122168c923906370209bcd0fcc
Arinji2 <arinjaydhar205@gmail.com>
2025-07-10 10:51:36 +0530

    Update migrations to add featured

---
+++ db/migrations/1752124276_updated_projects.go
@@ -0,0 +1,58 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_484305853")
+		if err != nil {
+			return err
+		}
+
+		// update collection data
+		if err := json.Unmarshal([]byte(`{
+			"indexes": [
+				"CREATE INDEX ` + "`" + `idx_NuMF4IXZDr` + "`" + ` ON ` + "`" + `projects` + "`" + ` (` + "`" + `featured` + "`" + `)"
+			]
+		}`), &collection); err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(5, []byte(`{
+			"hidden": false,
+			"id": "bool1007901140",
+			"name": "featured",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "bool"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_484305853")
+		if err != nil {
+			return err
+		}
+
+		// update collection data
+		if err := json.Unmarshal([]byte(`{
+			"indexes": []
+		}`), &collection); err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("bool1007901140")
+
+		return app.Save(collection)
+	})
+}
