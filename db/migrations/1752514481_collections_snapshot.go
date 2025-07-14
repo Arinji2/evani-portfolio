3eb4392fadd20c2396e214a0f9b3d61f155f9502
Arinji2 <arinjaydhar205@gmail.com>
2025-07-14 23:04:57 +0530

    Remove keys collection, add homepage boolean to art

--- db/migrations/1752514481_collections_snapshot.go
+++ db/migrations/1752514481_collections_snapshot.go
@@ -667,60 +667,6 @@ func init() {
 						"required": false,
 						"system": true,
 						"type": "bool"
-					},
-					{
-						"autogeneratePattern": "",
-						"hidden": false,
-						"id": "text1579384326",
-						"max": 255,
-						"min": 0,
-						"name": "name",
-						"pattern": "",
-						"presentable": false,
-						"primaryKey": false,
-						"required": false,
-						"system": false,
-						"type": "text"
-					},
-					{
-						"hidden": false,
-						"id": "file376926767",
-						"maxSelect": 1,
-						"maxSize": 0,
-						"mimeTypes": [
-							"image/jpeg",
-							"image/png",
-							"image/svg+xml",
-							"image/gif",
-							"image/webp"
-						],
-						"name": "avatar",
-						"presentable": false,
-						"protected": false,
-						"required": false,
-						"system": false,
-						"thumbs": null,
-						"type": "file"
-					},
-					{
-						"hidden": false,
-						"id": "autodate2990389176",
-						"name": "created",
-						"onCreate": true,
-						"onUpdate": false,
-						"presentable": false,
-						"system": false,
-						"type": "autodate"
-					},
-					{
-						"hidden": false,
-						"id": "autodate3332085495",
-						"name": "updated",
-						"onCreate": true,
-						"onUpdate": true,
-						"presentable": false,
-						"system": false,
-						"type": "autodate"
 					}
 				],
 				"fileToken": {
@@ -742,9 +688,9 @@ func init() {
 				"oauth2": {
 					"enabled": false,
 					"mappedFields": {
-						"avatarURL": "avatar",
+						"avatarURL": "",
 						"id": "",
-						"name": "name",
+						"name": "",
 						"username": ""
 					}
 				},
@@ -758,7 +704,7 @@ func init() {
 					"length": 8
 				},
 				"passwordAuth": {
-					"enabled": true,
+					"enabled": false,
 					"identityFields": [
 						"email"
 					]
@@ -781,64 +727,6 @@ func init() {
 					"duration": 259200
 				},
 				"viewRule": "id = @request.auth.id"
-			},
-			{
-				"createRule": null,
-				"deleteRule": null,
-				"fields": [
-					{
-						"autogeneratePattern": "[a-z0-9]{15}",
-						"hidden": false,
-						"id": "text3208210256",
-						"max": 15,
-						"min": 15,
-						"name": "id",
-						"pattern": "^[a-z0-9]+$",
-						"presentable": false,
-						"primaryKey": true,
-						"required": true,
-						"system": true,
-						"type": "text"
-					},
-					{
-						"autogeneratePattern": "",
-						"hidden": false,
-						"id": "text2324736937",
-						"max": 0,
-						"min": 0,
-						"name": "key",
-						"pattern": "",
-						"presentable": false,
-						"primaryKey": false,
-						"required": false,
-						"system": false,
-						"type": "text"
-					},
-					{
-						"autogeneratePattern": "",
-						"hidden": false,
-						"id": "text494360628",
-						"max": 0,
-						"min": 0,
-						"name": "value",
-						"pattern": "",
-						"presentable": false,
-						"primaryKey": false,
-						"required": false,
-						"system": false,
-						"type": "text"
-					}
-				],
-				"id": "pbc_2474845020",
-				"indexes": [
-					"CREATE UNIQUE INDEX ` + "`" + `idx_Xot8iw9h4R` + "`" + ` ON ` + "`" + `keys` + "`" + ` (` + "`" + `key` + "`" + `)"
-				],
-				"listRule": null,
-				"name": "keys",
-				"system": false,
-				"type": "base",
-				"updateRule": null,
-				"viewRule": null
 			},
 			{
 				"createRule": null,
@@ -911,10 +799,21 @@ func init() {
 						"required": false,
 						"system": false,
 						"type": "number"
+					},
+					{
+						"hidden": false,
+						"id": "bool1007901140",
+						"name": "featured",
+						"presentable": false,
+						"required": false,
+						"system": false,
+						"type": "bool"
 					}
 				],
 				"id": "pbc_484305853",
-				"indexes": [],
+				"indexes": [
+					"CREATE INDEX ` + "`" + `idx_NuMF4IXZDr` + "`" + ` ON ` + "`" + `projects` + "`" + ` (` + "`" + `featured` + "`" + `)"
+				],
 				"listRule": null,
 				"name": "projects",
 				"system": false,
@@ -993,12 +892,76 @@ func init() {
 						"required": false,
 						"system": false,
 						"type": "number"
+					},
+					{
+						"hidden": false,
+						"id": "bool3408188910",
+						"name": "homepage",
+						"presentable": false,
+						"required": false,
+						"system": false,
+						"type": "bool"
 					}
 				],
 				"id": "pbc_4078160933",
-				"indexes": [],
+				"indexes": [
+					"CREATE INDEX ` + "`" + `idx_wIz7voWP6E` + "`" + ` ON ` + "`" + `art` + "`" + ` (` + "`" + `homepage` + "`" + `)"
+				],
 				"listRule": null,
 				"name": "art",
+				"system": false,
+				"type": "base",
+				"updateRule": null,
+				"viewRule": null
+			},
+			{
+				"createRule": null,
+				"deleteRule": null,
+				"fields": [
+					{
+						"autogeneratePattern": "[a-z0-9]{15}",
+						"hidden": false,
+						"id": "text3208210256",
+						"max": 15,
+						"min": 15,
+						"name": "id",
+						"pattern": "^[a-z0-9]+$",
+						"presentable": false,
+						"primaryKey": true,
+						"required": true,
+						"system": true,
+						"type": "text"
+					},
+					{
+						"autogeneratePattern": "",
+						"hidden": false,
+						"id": "text1843675174",
+						"max": 0,
+						"min": 0,
+						"name": "description",
+						"pattern": "",
+						"presentable": false,
+						"primaryKey": false,
+						"required": true,
+						"system": false,
+						"type": "text"
+					},
+					{
+						"hidden": false,
+						"id": "date1163937275",
+						"max": "",
+						"min": "",
+						"name": "timeframe",
+						"presentable": false,
+						"required": true,
+						"system": false,
+						"type": "date"
+					}
+				],
+				"id": "pbc_112238207",
+				"indexes": [],
+				"listRule": null,
+				"name": "timeline",
 				"system": false,
 				"type": "base",
 				"updateRule": null,
