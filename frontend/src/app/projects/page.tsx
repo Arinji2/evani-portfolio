e46edb62297df3f5e5dd216fd44bb11c9378416d
Arinji2 <arinjaydhar205@gmail.com>
2025-07-17 19:58:50 +0530

    Finish Projects page

--- frontend/src/app/projects/page.tsx
+++ frontend/src/app/projects/page.tsx
@@ -1,34 +1,38 @@
-import type { WorkItem } from "./type";
-import WorkCard from "./work-card";
+import type { ProjectSchemaType } from "@/lib/data";
+import { getPB } from "@/lib/pb";
+import ProjectCard from "./work-card";
 
-export default function Projects() {
-	const initialData = Array.from({ length: 6 }).map((_, i) => {
-		return {
-			title: `Project ${i + 1}`,
-			description:
-				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-			randomHeight: Math.floor(Math.random() * (350 - 200 + 1)) + 200,
-		};
-	});
+const reorderForColumns = (
+	data: ProjectSchemaType[],
+	columnCount: number = 3,
+) => {
+	const itemsPerColumn = Math.ceil(data.length / columnCount);
+	const reordered = [];
 
-	const reorderForColumns = (data: WorkItem[], columnCount: number = 3) => {
-		const itemsPerColumn = Math.ceil(data.length / columnCount);
-		const reordered = [];
-
-		for (let col = 0; col < columnCount; col++) {
-			for (let row = 0; row < itemsPerColumn; row++) {
-				const index = row * columnCount + col;
-				if (index < data.length) {
-					reordered.push(data[index]);
-				}
+	for (let col = 0; col < columnCount; col++) {
+		for (let row = 0; row < itemsPerColumn; row++) {
+			const index = row * columnCount + col;
+			if (index < data.length) {
+				reordered.push(data[index]);
 			}
 		}
+	}
 
-		return reordered;
-	};
+	return reordered;
+};
+export default async function Projects() {
+	const pb = await getPB();
+	const results = await pb
+		.collection("projects")
+		.getFullList<ProjectSchemaType>({
+			sort: "-featured,-sort_order,-created_at",
+			expand: "image",
+		});
+
+	console.log(results);
 
 	return (
-		<div className=" flex h-fit w-full max-w-pageMax flex-col items-center justify-start gap-10 px-4 py-10 font-content md:gap-20 md:py-6">
+		<div className=" flex h-fit w-full max-w-pageMax flex-col items-center justify-start gap-10 px-4 py-10 font-content md:gap-20 md:py-6 md:pt-24">
 			<div className="flex h-fit w-full flex-col items-center justify-start gap-5 text-center">
 				<h1 className="font-bold font-title text-3xl text-black md:text-5xl">
 					My Projects/Publications
@@ -40,24 +44,21 @@ export default function Projects() {
 					aliquip ex ea commodo consequat.
 				</p>
 			</div>
-			<div className="hidden columns-3 xl:block">
-				{reorderForColumns(initialData, 3).map((item, key) => (
-					// biome-ignore lint/suspicious/noArrayIndexKey: Gonna fix when we have actual data
-					<WorkCard key={key} data={item} />
+			<div className="hidden h-full w-full columns-3 xl:block">
+				{reorderForColumns(results, 3).map((item) => (
+					<ProjectCard key={item.id} data={item} />
 				))}
 			</div>
 
-			<div className="hidden columns-2 md:block xl:hidden">
-				{reorderForColumns(initialData, 2).map((item, key) => (
-					// biome-ignore lint/suspicious/noArrayIndexKey: Gonna fix when we have actual data
-					<WorkCard key={key} data={item} />
+			<div className="hidden h-full w-full columns-2 md:block xl:hidden">
+				{reorderForColumns(results, 2).map((item) => (
+					<ProjectCard key={item.id} data={item} />
 				))}
 			</div>
 
-			<div className="block columns-1 md:hidden">
-				{reorderForColumns(initialData, 1).map((item, key) => (
-					// biome-ignore lint/suspicious/noArrayIndexKey: Gonna fix when we have actual data
-					<WorkCard key={key} data={item} />
+			<div className="block h-full w-full columns-1 md:hidden">
+				{reorderForColumns(results, 1).map((item) => (
+					<ProjectCard key={item.id} data={item} />
 				))}
 			</div>
 		</div>
--- frontend/src/app/projects/work-card.tsx
+++ frontend/src/app/projects/work-card.tsx
@@ -1,13 +1,20 @@
-import type { WorkItem } from "./type";
+import Image from "next/image";
+import type { ProjectSchemaType } from "@/lib/data";
+import { getProxyURL } from "@/lib/utils";
 
-export default function WorkCard({ data }: { data: WorkItem }) {
-	const { title, description, randomHeight } = data;
+export default function ProjectCard({ data }: { data: ProjectSchemaType }) {
+	const { title, description, expand } = data;
+	const { height, width, drive_id } = expand.image;
 	return (
 		<div className="mb-6 flex h-fit w-full break-inside-avoid flex-col items-start justify-start gap-4">
 			<div
-				className="w-full bg-neutral-600 "
-				style={{ height: `${randomHeight}px` }}
-			/>
+				className="relative h-auto w-full"
+				style={{
+					aspectRatio: `${width} / ${height}`,
+				}}
+			>
+				<Image src={getProxyURL(drive_id).toString()} alt="project" fill />
+			</div>
 			<h2 className="font-title text-3xl text-black">{title}</h2>
 			<p className="font-content font-light text-black text-xl">
 				{description}
